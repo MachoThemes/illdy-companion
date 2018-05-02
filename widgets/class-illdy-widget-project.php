4bb53631c99c717b836e12cd640eca62f4ba5ebc
cristianraiber <cristian@machothemes.com>
2018-05-02 17:22:09 +0300

    added projects video field

--- widgets/class-illdy-widget-project.php
+++ widgets/class-illdy-widget-project.php
@@ -8,8 +8,8 @@ class Illdy_Widget_Project extends WP_Widget {
 	function __construct() {
 		parent::__construct(
 			'illdy_project', __( '[Illdy] - Project', 'illdy-companion' ), array(
-				'description' => __( 'Add this widget in "Front page - Projects Sidebar".', 'illdy-companion' ),
-			)
+				               'description' => __( 'Add this widget in "Front page - Projects Sidebar".', 'illdy-companion' ),
+			               )
 		);
 
 		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
@@ -34,13 +34,17 @@ class Illdy_Widget_Project extends WP_Widget {
 	public function widget( $args, $instance ) {
 		echo $args['before_widget'];
 
-		$lightbox = get_theme_mod( 'illdy_projects_lightbox', false );
+		$url       = '';
+		$video_url = '';
+		$lightbox  = get_theme_mod( 'illdy_projects_lightbox', false );
 
 		$defaults = array(
 			'title' => '',
 			'url'   => '',
 			'image' => '',
+			'video' => '',
 		);
+
 		$instance = wp_parse_args( $instance, $defaults );
 
 		$image_id                 = illdy_get_image_id_from_image_url( $instance['image'] );
@@ -51,23 +55,23 @@ class Illdy_Widget_Project extends WP_Widget {
 		if ( '' == $instance['url'] && ! $lightbox ) {
 			$class .= ' no-url';
 		}
-		$attr = '';
 
 		if ( $lightbox ) {
-			if ( $image_id ) {
+			if ( $image_id && empty( $instance['video'] ) ) {
 				$url = wp_get_attachment_image_src( $image_id, 'full' );
 				$url = $url[0];
+			} elseif ( ! empty( $instance['video'] ) ) {
+				$url = $instance['video'];
 			} else {
 				$url = $instance['image'];
 			}
-
-						$class .= ' fancybox';
-			$attr               = ' rel="projects-gallery"';
 		} else {
 			$url = $instance['url'];
 		}
 
-		$output = '<a href="' . esc_url( $url ) . '" title="' . esc_attr( $instance['title'] ) . '" class="' . $class . '"' . $attr . ' style="background-image: url(' . ( $image_id ? esc_url( $get_attachment_image_src[0] ) : esc_url( $instance['image'] ) ) . ');"><span class="project-overlay"></span></a>';
+		$output  = '<a href="' . esc_attr( $url ) . '" data-caption="' . esc_attr( $instance['title'] ) . '" class="' . esc_attr( $class ) . '" data-fancybox="gallery"><span class="project-overlay"></span>';
+		$output .= '<img src="' . esc_url( ( $image_id ? esc_url( $get_attachment_image_src[0] ) : esc_url( $instance['image'] ) ) ) . '"/>';
+		$output .= '</a>';
 
 		echo $output;
 
@@ -86,6 +90,7 @@ class Illdy_Widget_Project extends WP_Widget {
 		$defaults = array(
 			'title' => __( '[Illdy] - Project', 'illdy-companion' ),
 			'url'   => '',
+			'video' => '',
 			'image' => get_template_directory_uri() . '/layout/images/front-page/front-page-project-1.jpg',
 		);
 		$instance = wp_parse_args( $instance, $defaults );
@@ -101,6 +106,12 @@ class Illdy_Widget_Project extends WP_Widget {
 			<input type="text" class="widefat custom_media_url_<?php echo $this->get_field_id( 'image' ); ?>" name="<?php echo $this->get_field_name( 'image' ); ?>" id="<?php echo $this->get_field_id( 'image' ); ?>" value="<?php echo $instance['image']; ?>" style="margin-top:5px;">
 			<input type="button" class="button button-primary custom_media_button" id="custom_media_button_service" data-fieldid="<?php echo $this->get_field_id( 'image' ); ?>" name="<?php echo $this->get_field_name( 'image' ); ?>" value="<?php _e( 'Upload Image', 'illdy-companion' ); ?>" style="margin-top: 5px;">
 		</p>
+
+		<p>
+			<label for="<?php echo $this->get_field_name( 'video' ); ?>"><?php _e( 'Video: (YouTube or Vimeo only)', 'illdy-companion' ); ?></label>
+			<input type="text" class="widefat" name="<?php echo $this->get_field_name( 'video' ); ?>" id="<?php echo $this->get_field_id( 'video' ); ?>" value="<?php echo $instance['video']; ?>" style="margin-top:5px;">
+		</p>
+
 
 		<p>
 			<label for="<?php echo $this->get_field_id( 'url' ); ?>"><?php _e( 'URL:', 'illdy-companion' ); ?></label>
@@ -123,6 +134,7 @@ class Illdy_Widget_Project extends WP_Widget {
 		$instance          = array();
 		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? sanitize_text_field( $new_instance['title'] ) : '';
 		$instance['image'] = ! empty( $new_instance['image'] ) ? esc_url_raw( $new_instance['image'] ) : '';
+		$instance['video'] = ! empty( $new_instance['video'] ) ? esc_url_raw( $new_instance['video'] ) : '';
 		$instance['url']   = ( ! empty( $new_instance['url'] ) ? esc_url_raw( $new_instance['url'] ) : '' );
 
 		return $instance;
@@ -133,4 +145,5 @@ class Illdy_Widget_Project extends WP_Widget {
 function illdy_register_widget_project() {
 	register_widget( 'Illdy_Widget_Project' );
 }
+
 add_action( 'widgets_init', 'illdy_register_widget_project' );
